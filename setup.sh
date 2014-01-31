6d0051b0d76b82fda89ce978643ff4c3910109b3
Valentin Tolmer <valentin.tolmer@gmail.com>
2014-02-01 00:51:51 +0100

    setup: nice messages

--- setup.sh
+++ setup.sh
@@ -5,14 +5,11 @@ echo "Loading config folder in ${CONFIG=$HOME/config}"
 link () {
   if ! [ -e $HOME/$2 ]; then
     ln -s $CONFIG/$1 $HOME/$2
+    echo "$HOME/$2 --> $CONFIG/$1"
   fi
 }
 
-copy () {
-  if ! [ -e $HOME/$2 ]; then
-    cp $CONFIG/sample/$1 $HOME/$2
-  fi
-}
+echo "Linking to config files..."
 
 link vimrc .vimrc
 link zshrc .zshrc-generic
@@ -26,11 +23,22 @@ link vimperatorrc .vimperatorrc
 link Xdefaults .Xdefaults
 link oh-my-zsh .oh-my-zsh
 
+copy () {
+  if ! [ -e $HOME/$2 ]; then
+    cp $CONFIG/sample/$1 $HOME/$2
+    echo "$CONFIG/sample/$1 --> $HOME/$2"
+  fi
+}
+
+echo "Copying defaults..."
+
 copy gitconfig .gitconfig
 copy jnewsrc .jnewsrc
 copy xinitrc .xinitrc
 copy zshrc .zshrc
 
+echo "Setting up lesskey..."
 lesskey $CONFIG/lesskey
+
 
 echo "Configuration successful!"
