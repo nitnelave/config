05fad536b43cce9425db6676e3da8a782a8c451f
Valentin Tolmer <valentin@tolmer.fr>
2024-01-30 15:40:40 +0100

    nvim: fix colorscheme for floating windows

--- nvim/lua/user/plugins/colorscheme.lua
+++ nvim/lua/user/plugins/colorscheme.lua
@@ -5,7 +5,17 @@ return {
     priority = 1000,
     config = function(plugin)
       vim.opt.rtp:append(plugin.dir .. "/vim")
-      vim.cmd [[ colorscheme onehalfdark ]]
+      vim.cmd [[
+        colorscheme onehalfdark
+        hi clear NormalFloat
+        hi link NormalFloat Normal
+        hi clear FloatBorder
+        hi FloatBorder ctermfg=188 ctermbg=237 guifg=#dcdfe4 guibg=#313640
+        hi clear Pmenu
+        hi link Pmenu Visual
+        hi clear PmenuSel
+        hi link PmenuSel Cursor
+      ]]
     end,
   },
 }
