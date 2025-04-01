5da004953020a97ef635f1765614860a0c329e7d
Valentin Tolmer <valentin@tolmer.fr>
2025-03-31 22:09:44 -0500

    wezterm: Add search

--- wezterm.lua
+++ wezterm.lua
@@ -7,5 +7,18 @@ config.font = wezterm.font 'RobotoMono Nerd Font'
 config.enable_tab_bar = false
 config.scrollback_lines = 100000
 
+config.keys = {
+  {
+    key = 'f',
+    mods = 'CMD',
+    action = wezterm.action.Search { CaseInSensitiveString = "" },
+  },
+  {
+    key = 'f',
+    mods = 'SHIFT|CTRL',
+    action = wezterm.action.Search { CaseInSensitiveString = "" },
+  },
+}
+
 
 return config
